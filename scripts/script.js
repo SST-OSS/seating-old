5b0cf96898d9c1ace5ec9b04e496deee1c6776d3
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-08-10 11:05:43 +0530

    add dotenv

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,40 @@
-const { getFirestore, setDoc, doc } = require("firebase/firestore");
-const { initializeApp } = require("firebase/app");
-const fs = require("fs");
+const { getFirestore, setDoc, doc } = require('firebase/firestore');
+const { initializeApp } = require('firebase/app');
+const { config } = require('dotenv');
+config();
+const fs = require('fs');
 const firebaseConfig = {
-// ...
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
+	measurementId: process.env.REACT_APP_measurementId,
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const batches = [];
 
-const data = fs.readFileSync("batch1.txt");
-const batch1 = data.toString().split("\n");
+const data = fs.readFileSync('batch1.txt');
+const batch1 = data.toString().split('\n');
 batch1.forEach((name) => {
-  batches.push({ name, batch: "A" });
+	batches.push({ name, batch: 'A' });
 });
 
-const data2 = fs.readFileSync("batch2.txt");
-const batch2 = data2.toString().split("\n");
+const data2 = fs.readFileSync('batch2.txt');
+const batch2 = data2.toString().split('\n');
 batch2.forEach((name) => {
-  batches.push({ name, batch: "B" });
+	batches.push({ name, batch: 'B' });
 });
 
-const data3 = fs.readFileSync("batch3.txt");
-const batch3 = data3.toString().split("\n");
+const data3 = fs.readFileSync('batch3.txt');
+const batch3 = data3.toString().split('\n');
 batch3.forEach((name) => {
-  console.log({ name });
-  batches.push({ name, batch: "C" });
+	console.log({ name });
+	batches.push({ name, batch: 'C' });
 });
 
 (async () => {
-  await setDoc(doc(db, "students", "1Xb39GEv03AIggNNcJYC"), { data: batches });
+	await setDoc(doc(db, 'students', '1Xb39GEv03AIggNNcJYC'), { data: batches });
 })();
