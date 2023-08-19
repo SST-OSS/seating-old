88316994b88739b0184aa077c9676ecfd5542b76
partharora1610 <partharora16102001@gmail.com>
2023-08-20 00:09:53 +0530

    index updated

--- client/src/util/data.js
+++ client/src/util/data.js
@@ -1,7 +1,7 @@
 export const structure = [
   { rowCount: 9, colCount: 3, colName: "G", startIndex: 0 },
   { rowCount: 9, colCount: 4, colName: "F", startIndex: 27 },
-  { rowCount: 8, colCount: 3, colName: "E", startIndex: 64 },
+  { rowCount: 8, colCount: 3, colName: "E", startIndex: 63 },
   { rowCount: 9, colCount: 3, colName: "D", startIndex: 87 },
   { rowCount: 9, colCount: 3, colName: "C", startIndex: 114 },
   { rowCount: 9, colCount: 3, colName: "B", startIndex: 141 },
