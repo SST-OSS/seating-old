34643159cc594e3308efa77822126a07445d08a4
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-08-21 11:28:47 +0530

    Added BSM and fixed font case

--- client/src/components/Seat/Seat.js
+++ client/src/components/Seat/Seat.js
@@ -1,38 +1,37 @@
-import React, { useEffect, useState } from "react";
-import modules from "./Seat.module.css";
+import React, { useEffect, useState } from 'react';
+import modules from './Seat.module.css';
 
 const Seat = ({ student, searchText }) => {
-  const [highlight, setHighlight] = useState(false);
+	const [highlight, setHighlight] = useState(false);
+	useEffect(() => {
+		if (searchText.length === 0) {
+			setHighlight(false);
+		}
 
-  useEffect(() => {
-    if (searchText.length === 0) {
-      setHighlight(false);
-    }
+		if (
+			!student?.name.toLowerCase().includes(searchText.toLowerCase()) &&
+			searchText.length !== 0
+		) {
+			setHighlight(false);
+		}
 
-    if (
-      !student?.name.toLowerCase().includes(searchText.toLowerCase()) &&
-      searchText.length !== 0
-    ) {
-      setHighlight(false);
-    }
+		if (
+			student?.name.toLowerCase().includes(searchText.toLowerCase()) &&
+			searchText.length >= 3
+		) {
+			setHighlight(true);
+		}
+	}, [searchText, student]);
 
-    if (
-      student?.name.toLowerCase().includes(searchText.toLowerCase()) &&
-      searchText.length >= 3
-    ) {
-      setHighlight(true);
-    }
-  }, [searchText, student]);
+	const highlightClass = highlight ? `seatHigh` : 'seatNormal';
 
-  const highlightClass = highlight ? `seatHigh` : "seatNormal";
-
-  return (
-    <div>
-      <div className={modules.seat}>
-        <p className={highlightClass}>{student?.name}</p>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className={modules.seat}>
+				<p className={highlightClass}>{student?.name.toLowerCase()}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Seat;
