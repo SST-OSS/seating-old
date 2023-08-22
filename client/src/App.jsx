06242eb5467e0af712b9121b06a97f4791715291
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-08-22 11:45:05 +0530

    fixed the responsive issue with header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,40 @@
-import getStudent from "./util/getStudent";
-import generateNewArray from "./util/generateNewArray";
-import { useEffect, useState } from "react";
-import Header from "./components/Header/Header";
-import SeatContainer from "./components/SeatContainer/SeatContainer";
-import ScreenBox from "./components/UI/ScreenBox";
+import getStudent from './util/getStudent';
+import generateNewArray from './util/generateNewArray';
+import { useEffect, useState } from 'react';
+import Header from './components/Header/Header';
+import SeatContainer from './components/SeatContainer/SeatContainer';
+import ScreenBox from './components/UI/ScreenBox';
 
 function App() {
-  const [studentData, setStudentData] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
+	const [studentData, setStudentData] = useState([]);
+	const [searchInput, setSearchInput] = useState('');
 
-  useEffect(() => {
-    (async () => {
-      const data = await getStudent();
-      const newData = generateNewArray(data.data);
-      setStudentData(newData);
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			const data = await getStudent();
+			const newData = generateNewArray(data.data);
+			setStudentData(newData);
+		})();
+	}, []);
 
-  if (studentData.length === 0) {
-    return <div>Loading...</div>;
-  }
+	if (studentData.length === 0) {
+		return <div>Loading...</div>;
+	}
 
-  const captureSearchInput = (input) => {
-    setSearchInput(input);
-  };
+	const captureSearchInput = (input) => {
+		setSearchInput(input);
+	};
 
-  return (
-    <div className="flex flex-col">
-      <Header value={searchInput} onCaptureInput={captureSearchInput} />
-      <ScreenBox />
-      <SeatContainer data={studentData} searchText={searchInput} />
-    </div>
-  );
+	return (
+		<div className="flex flex-col">
+			<div className="flex flex-col" id="header">
+				<Header value={searchInput} onCaptureInput={captureSearchInput} />
+				<ScreenBox />
+			</div>
+
+			<SeatContainer data={studentData} searchText={searchInput} />
+		</div>
+	);
 }
 
 export default App;
--- client/src/components/SeatContainer/SeatContainer.jsx
+++ client/src/components/SeatContainer/SeatContainer.jsx
@@ -1,24 +1,19 @@
-import React from "react";
-import Column from "../Column/Column";
-import { structure } from "../../util/data";
+import React from 'react';
+import Column from '../Column/Column';
+import { structure } from '../../util/data';
 
 const SeatContainer = ({ data, searchText }) => {
-  return (
-    <div className="gap-20 p-4">
-      <div className="flex items-start gap-20">
-        {structure.map((column, idx) => {
-          return (
-            <Column
-              {...column}
-              students={data}
-              searchText={searchText}
-              key={idx}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+	return (
+		<div className="gap-20 p-4" id="seat-container">
+			<div className="flex items-start gap-20">
+				{structure.map((column, idx) => {
+					return (
+						<Column {...column} students={data} searchText={searchText} key={idx} />
+					);
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default SeatContainer;
