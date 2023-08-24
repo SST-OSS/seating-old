d4a3d250b9a7efa0fb2a652c5819d8e36d93df63
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-08-24 15:18:57 +0530

    fixed the height of app (#23)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,39 @@
-import getStudent from "./util/getStudent";
-import generateNewArray from "./util/generateNewArray";
-import { useEffect, useState } from "react";
-import Header from "./components/Header/Header";
-import SeatContainer from "./components/SeatContainer/SeatContainer";
+import getStudent from './util/getStudent';
+import generateNewArray from './util/generateNewArray';
+import { useEffect, useState } from 'react';
+import Header from './components/Header/Header';
+import SeatContainer from './components/SeatContainer/SeatContainer';
 
 function App() {
-  const [studentData, setStudentData] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
+	const [studentData, setStudentData] = useState([]);
+	const [searchInput, setSearchInput] = useState('');
 
-  useEffect(() => {
-    (async () => {
-      const data = await getStudent();
-      const newData = generateNewArray(data.data);
-      setStudentData(newData);
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			const data = await getStudent();
+			const newData = generateNewArray(data.data);
+			setStudentData(newData);
+		})();
+	}, []);
 
-  if (studentData.length === 0) {
-    return (
-      <div className="w-screen h-screen flex flex-col items-center justify-center text-white font-bold text-5xl">
-        Loading...
-      </div>
-    );
-  }
+	if (studentData.length === 0) {
+		return (
+			<div className="w-screen flex flex-col items-center justify-center text-white font-bold text-5xl h-screen">
+				Loading...
+			</div>
+		);
+	}
 
-  const captureSearchInput = (input) => {
-    setSearchInput(input);
-  };
+	const captureSearchInput = (input) => {
+		setSearchInput(input);
+	};
 
-  return (
-    <div className="flex flex-col overflow-hidden bg-stone-900">
-      <Header value={searchInput} onCaptureInput={captureSearchInput} />
-      <SeatContainer data={studentData} searchText={searchInput} />
-    </div>
-  );
+	return (
+		<div className="flex flex-col overflow-hidden bg-stone-900 h-screen">
+			<Header value={searchInput} onCaptureInput={captureSearchInput} />
+			<SeatContainer data={studentData} searchText={searchInput} />
+		</div>
+	);
 }
 
 export default App;
