1925922300029a59dcddfd0d95ad1c77e74f8332
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-08-24 15:33:39 +0530

    Fixed the scroll issue

--- client/src/components/SeatContainer/SeatContainer.jsx
+++ client/src/components/SeatContainer/SeatContainer.jsx
@@ -1,22 +1,17 @@
-import React from "react";
-import Column from "../Column/Column";
-import { structure } from "../../util/data";
+import React from 'react';
+import Column from '../Column/Column';
+import { structure } from '../../util/data';
 
 const SeatContainer = ({ data, searchText }) => {
-  return (
-    <div className="flex items-start gap-20 overflow-x-scroll overflow-y-hidden p-6 mt-[150px]  md:mt-[170px]">
-      {structure.map((column, idx) => {
-        return (
-          <Column
-            {...column}
-            students={data}
-            searchText={searchText}
-            key={idx}
-          />
-        );
-      })}
-    </div>
-  );
+	return (
+		<div className="flex items-start gap-20 overflow-x-scroll  p-6 mt-[150px]  md:mt-[170px]">
+			{structure.map((column, idx) => {
+				return (
+					<Column {...column} students={data} searchText={searchText} key={idx} />
+				);
+			})}
+		</div>
+	);
 };
 
 export default SeatContainer;
