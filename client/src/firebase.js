02131e6339eaeb32880772db7debde878275db30
Arth <probablyarth@gmail.com>
2023-08-29 14:09:42 +0530

    chore: clean code:

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,7 +8,6 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
