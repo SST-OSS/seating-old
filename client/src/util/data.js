84498a679ba63284f20b0031c67b232b631be3f8
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-09-02 15:30:27 +0530

    Fixed the start index issue (#34)
    * Fixed the issue with space
    
    * Fixed the start index issue Column E

--- client/src/util/data.js
+++ client/src/util/data.js
@@ -1,7 +1,7 @@
 export const structure = [
 	{ rowCount: 9, colCount: 3, colName: 'G', startIndex: 0 },
 	{ rowCount: 9, colCount: 4, colName: 'F', startIndex: 27 },
-	{ rowCount: 9, colCount: 3, colName: 'E', startIndex: 66 },
+	{ rowCount: 9, colCount: 3, colName: 'E', startIndex: 63 },
 	{ rowCount: 8, colCount: 3, colName: 'D', startIndex: 90 },
 	{ rowCount: 9, colCount: 3, colName: 'C', startIndex: 114 },
 	{ rowCount: 9, colCount: 3, colName: 'B', startIndex: 141 },
